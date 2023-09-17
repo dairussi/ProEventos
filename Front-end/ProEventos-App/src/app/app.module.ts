c3879ee402797f89b9265c399e4614f04c0d7776
Daiana Russi <daianarussi.agroconsult@gmail.com>
2023-09-16 21:37:56 -0300

    seção 5

--- Front-end/ProEventos-App/src/app/app.module.ts
+++ Front-end/ProEventos-App/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 
@@ -18,8 +18,9 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- Front-end/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front-end/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,18 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
+  viewItem: boolean = false;
+  private _filtroLista: string = '';
+  public eventosFiltrados: any = [];
 
-  constructor(private http: HttpClient) {}
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos();
   }
 
+  view() {
+    this.viewItem = !this.viewItem;
+  }
+
   public getEventos(): void {
-    this.http.get('https://localhost:7168/api/Eventos').subscribe(
-      (response) => (this.eventos = response),
-      (error) => console.log(error)
+    this.http.get('https://localhost:7168/api/Eventos').subscribe({
+      next: (response) => { (this.eventos = response), (this.eventosFiltrados = this.eventos) },
+      error: (error) => console.log(error)
+    }
     );
   }
 }
