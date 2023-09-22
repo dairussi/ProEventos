1ad397dffabbc658d295b6595bba96948a2eec2f
Daiana Russi <daianarussi.agroconsult@gmail.com>
2023-09-22 15:08:33 -0300

    secao 6 inicio

---
+++ Back-end/src/ProEventos.Domain/PalestranteEvento.cs
@@ -0,0 +1,8 @@
+namespace ProEventos.Domain
+{
+    public class PalestranteEvento
+    {
+        public int PalestranteId { get; set; }
+        public PalestranteEvento Palestrante MyProperty { get; set; }
+}
+}
